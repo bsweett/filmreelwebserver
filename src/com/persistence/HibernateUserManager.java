e3ed423a32236f77eea08f88a88077ae71de796e
Ben Sweett <bensweett@cmail.carleton.ca>
2013-11-13 15:41:58 -0500

    Updates

--- src/com/persistence/HibernateUserManager.java
+++ src/com/persistence/HibernateUserManager.java
@@ -28,6 +28,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 
 
+
 import org.hibernate.HibernateException;
 import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Query;
@@ -241,7 +242,7 @@ public class HibernateUserManager extends
 	}
 	
 	@SuppressWarnings("unchecked")
-	public synchronized User getUserByEmailAddress(byte[] emailAddress) {
+	public synchronized User getUserByEmailAddress(byte[] bs) {
 		
 		Session session = null;
 		Transaction transaction = null;
@@ -249,7 +250,7 @@ public class HibernateUserManager extends
 			session = HibernateUtil.getCurrentSession();
 			transaction = session.beginTransaction();
 			Query query = session.createQuery(SELECT_USER_WITH_EMAIL_ADDRESS);
-			query.setParameter("email", emailAddress);
+			query.setParameter("email", bs);
 			List<User> users = query.list();
 			transaction.commit();
 
